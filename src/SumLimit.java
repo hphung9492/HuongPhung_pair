595822f5bed12401212275494521ed74b46a749f
hpark4 <hpark4@gmail.com>
2022-06-22 13:07:44 -0400

    one file added and commented..

--- src/SumLimit.java
+++ src/SumLimit.java
@@ -7,7 +7,7 @@
  * Given 2 non-negative ints, a and b,
  * return their sum, so long as the
  * sum has the same number of digits as a.
- * If the sum has more digits than a, just return a without b. .
+ * If the sum has more digits than a, just return a without b. ..
  */
 public class SumLimit {
 
