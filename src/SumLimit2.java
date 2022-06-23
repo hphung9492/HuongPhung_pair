14dd6e9a3220486a5505f23bd81d622eb10866d2
hphung9492 <106184699+hphung9492@users.noreply.github.com>
2022-06-23 15:01:49 -0400

    Update SumLimit2.java

--- src/SumLimit2.java
+++ src/SumLimit2.java
@@ -4,5 +4,45 @@
 public class SumLimit2 {
     //complete this version 2.
     //this time you DO NOT convert the integer values to String values.
+    
+    public static void main(String[] args) {
+        SumLimit2 sl = new SumLimit2();
+        System.out.println(sl.sumLimit(2, 3)); //this will yield 5
+        System.out.println(sl.sumLimit(123,1));//this will be 124
+        System.out.println(sl.sumLimit(23,80));//this will be 23
+    }
+
+    public int sumLimit(int a, int b) {
+        int sum = 0;
+        int counta = 0;
+        int countsum = 0;
+        int adigits = a;
+        while (adigits!=0)
+        {
+            adigits = adigits/10;
+            counta++;
+        }
+        while (sum<a)
+        {
+            sum += a+b;
+            int sumdigits = sum;
+            while(sumdigits!=0)
+            {
+                sumdigits = sumdigits/10;
+                countsum++;
+            }
+            if (countsum<=counta)
+            {
+                return sum;
+            }
+            else {
+                return a;
+            }
+        }
+        return sum;
+
+        //use while loop to complete this method.  You can use multiple loops, if you need.
+
+    }
 
 }
