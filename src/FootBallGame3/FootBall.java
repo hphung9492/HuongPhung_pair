3e23eea8ff0c4b51b6f6fcd85ac0e3efef2f9c16
hpark4 <hpark4@gmail.com>
2022-07-11 09:58:34 -0400

    third version design task added

--- src/FootBallGame3/FootBall.java
+++ src/FootBallGame3/FootBall.java
@@ -1,4 +1,16 @@
 package FootBallGame3;
 
 public class FootBall {
+    //attributes such as teamName, quarters, score.
+    //you can create Quarter class and include Quarter type object for example
+    //Quarter q1;
+    //you can create Team class and include Team type object for example
+    //Team hTeam or vTeam or Team tm ;
+
+    //constructors
+
+    //getters, setters, and toString method
+
+    //other methods to store scores
+
 }
--- src/FootBallGame3/Main.java
+++ src/FootBallGame3/Main.java
@@ -1,4 +1,7 @@
 package FootBallGame3;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
