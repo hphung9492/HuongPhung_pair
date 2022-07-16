7d4ba8c8780009aff1abf43bbd7b343cd9d2eb77
hphung9492 <106184699+hphung9492@users.noreply.github.com>
2022-07-16 14:31:52 -0400

    Update Quarter.java

--- src/FootBallGame5/Quarter.java
+++ src/FootBallGame5/Quarter.java
@@ -1,5 +1,5 @@
 package FootBallGame5;
-
+import java.util.Scanner;
 /**
  * this class will take care of quarters of each team
  * this class also need to create a method to compute the score and add the score.
