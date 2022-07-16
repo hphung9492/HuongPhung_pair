215d299e24a371d0afc229f86a8ac3e9ba7e42e8
hphung9492 <106184699+hphung9492@users.noreply.github.com>
2022-07-16 14:27:50 -0400

    Update Main.java

--- src/FootBallGame5/Main.java
+++ src/FootBallGame5/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         Teams[] teams = new Teams[2];
+        Teams t = new Teams();
+        t.setTeam1();
+        t.setTeam2();
+        Quarter q = new Quarter();
+        q.q();
+        System.out.println(q.winner(t.getTeam1(), t.getTeam2()));
     }
 }
