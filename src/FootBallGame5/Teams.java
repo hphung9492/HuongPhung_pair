2ab9759caed685219b7de420e00e0165b38bba6e
hphung9492 <106184699+hphung9492@users.noreply.github.com>
2022-07-16 14:31:31 -0400

    Update Teams.java

--- src/FootBallGame5/Teams.java
+++ src/FootBallGame5/Teams.java
@@ -1,9 +1,37 @@
 package FootBallGame5;
-
+import java.util.Scanner;
 public class Teams {
-    String teamIdentity; //whether the home team or visitor team.
-    String teamName;
+   int teamIdentity; //whether the home team or visitor team.
+    String team1;
+    String team2;
     Quarter[] quarters = new Quarter[4];
+    Scanner in = new Scanner(System.in);
+    public void setTeam1() {
+        System.out.println("Please enter the home's name: ");
+        this.team1 = in.nextLine();
+    }
+
+    public String getTeam1() {
+        return team1;
+    }
+
+    public void setTeam2()
+    {
+        System.out.println("Please enter the visit's name: ");
+        this.team2 = in.nextLine();
+    }
+
+    public String getTeam2() {
+        return team2;
+    }
+
+    public void setTeamIdentity() {
+        System.out.println("Please enter the team that scored. 1 for team 1 and 2 for team 2.");
+        this.teamIdentity = in.nextInt();
+    }
+    public int getTeamIdentity() {
+        return teamIdentity;
+    }
 
 
 
